e01f7f621a64dd5d301d9af17f4c6daabf92e3dc
Fabian Sarango <fasagu99@gmail.com>
2022-04-26 23:52:55 +0100

    adding code for creating password

--- Assets/script.js
+++ Assets/script.js
@@ -26,3 +26,160 @@ function passwordLengthChecker() {
     passwordLengthChecker();
   }
 }
+
+
+//This checks if the user wants capital , lower or both  casing.
+function casing() {
+  let casingType = prompt("Do you want uppercase, lowercase or both?");
+// Alternative would be using the bitwise value of each character to create the arrays. I will attempt this week
+  const alphabetCapital = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const alphabetLower = alphabetCapital.map(function (x) {
+    return x.toLowerCase();
+  });
+
+  let upperCaseOptions = ["uppercase", "upper case", "upper"];
+  let lowerCaseOptions = ["lowercase", "lower case", "lower"];
+
+  const bothTypes = alphabetCapital.concat(alphabetLower);
+
+  if (casingType.toLowerCase() == "both"){
+    return bothTypes;
+  } else if (upperCaseOptions.includes(casingType.toLowerCase())){
+    return alphabetCapital;
+  } else if (lowerCaseOptions.includes(casingType.toLowerCase())) {
+    return alphabetLower;
+  } else {
+    alert("Wrong input value");
+    casing();
+  }
+}
+
+//NUMERICS
+function numbering() {
+  let numeric = prompt("Do you want numbers? YES/NO?");
+  const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  if (numeric.toLowerCase() == "yes") {
+    return numberArray;
+  }
+  if (numeric.toLowerCase() == "no") {
+    return [];
+  } else {
+    alert('Wrong input, type "YES" or "NO"');
+    numbering();
+  }
+}
+
+// SPECIAL CHARACTERS
+function specialCharactersChecker() {
+  let specialChar = prompt("Do you want special characters? YES/NO");
+
+  const specialCharacters = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  if (specialChar.toLowerCase() == "yes") {
+    return specialCharacters;
+  } else if (specialChar.toLowerCase() == "no") {
+    return [];
+  } else {
+    alert('Type "YES" or "NO"');
+    specialCharactersChecker();
+  }
+}
+
+// Random array element selector
+function randomSelector(array) {
+  let index = Math.floor(Math.random() * array.length);
+  let value = array[index];
+  return value;
+}
+
+// This function checks if the the password contains at least one from each type.
+function isTrue(arr, arr2, i) {
+  if (arr.every((i) => arr2.includes(i))) {
+    i--;
+  }
+}
+
+// This generates the password by combining the arrays from the different choices that the user made.
+function generatePassword() {
+  // This arrays will contain the values that the user selected.
+  let passwordLengthValue = passwordLengthChecker();
+  let casingType = casing();
+  let numbersAdder = numbering();
+  let specialCharactersArray = specialCharactersChecker();
+
+  // Global array combining choices. If the user didnt want numbers, the array added would have no values.
+  let globalOptions = casingType.concat(numbersAdder, specialCharactersArray);
+
+  // 
+  let finalPassword = [];
+  for (let i = 0; i < passwordLengthValue; i++) {
+    finalPassword.push(randomSelector(globalOptions));
+    // Before the password reaches the desired length, we will check if it contains at least one character from each type.
+    if (i == passwordLengthValue - 1) {
+      isTrue(finalPassword, casingType, i);
+      isTrue(finalPassword, numbersAdder, i);
+      isTrue(finalPassword, specialCharactersArray, i);
+    }
+  }
+  return finalPassword.join("");
+}
